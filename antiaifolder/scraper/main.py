4e1b4c0f4db49f67b91b82cbb74fe04e170a10c0
elkelboub4th-lab <elkelboub4th@gmail.com>
2026-05-05 21:00:45 +0100

    Update main.py

--- antiaifolder/scraper/main.py
+++ antiaifolder/scraper/main.py
@@ -7,7 +7,7 @@ import threading
 from http.server import BaseHTTPRequestHandler, HTTPServer
 from dotenv import load_dotenv
 from supabase import create_client, Client
-from playwright.sync_api import sync_playwright
+#from playwright.sync_api import sync_playwright
 from groq import Groq
 
 load_dotenv()
